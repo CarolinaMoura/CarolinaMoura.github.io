4a88ee43229697805c47967bf3c38d77ee673e74
Carolina Moura <carolinamouravalle@yahoo.com>
2025-05-02 16:16:20 -0400

    Change user channel to restart everything

--- consts.js
+++ consts.js
@@ -1,2 +1,3 @@
-export const USER_CHANNEL = ['c9e035fa-6d86-438e-bb83-6013ba095738', 'designftw-2025-studio2'];
+// export const USER_CHANNEL = ['c9e035fa-6d86-438e-bb83-6013ba095738', 'designftw-2025-studio2'];
+export const USER_CHANNEL = ['d6181487-ac58-4ffc-8c3f-f2d0ab1f12fb'];
 export const ANIMALS = ["cat", "chicken", "panda", "rabbit", "koala", "sea-lion", "giraffe", "puffer-fish", "penguin", "sloth", "dragon", "dog", "hen", "weasel"];
