12e3f390169bb7758f4570e52abfad3b741c884a
Carolina Moura <carolinamouravalle@yahoo.com>
2025-05-05 15:16:27 -0400

    Change user channel

--- consts.js
+++ consts.js
@@ -1,4 +1,4 @@
 // export const USER_CHANNEL = ['c9e035fa-6d86-438e-bb83-6013ba095738', 'designftw-2025-studio2'];
-export const USER_CHANNEL = ['1d6cb93a-316b-4fc9-83ed-fe401ac59076'];
+export const USER_CHANNEL = ['39fc3eb5-25fb-471b-bc5c-a7c6acf9bdb0'];
 export const DESIGN_CHANNEL = "designftw-2025-studio2";
 export const ANIMALS = ["cat", "chicken", "panda", "rabbit", "koala", "sea-lion", "giraffe", "puffer-fish", "penguin", "sloth", "dragon", "dog", "hen", "weasel"];
