cd009aa5dc5b27646be895585b20f1160c07cae9
Carolina Moura <carolinamouravalle@yahoo.com>
2025-05-07 12:09:32 -0400

    Add reminder channel

--- consts.js
+++ consts.js
@@ -1,3 +1,3 @@
 export const USER_CHANNEL = ['e7cd2aca-b6dd-4a51-91d2-07b4ca24a774'];
-// export const DESIGN_CHANNEL = "designftw-2025-studio2";
+export const REMINDER_CHANNEL = ['dff7653f-c111-4fb3-99af-2c695a8717cb'];
 export const ANIMALS = ["cat", "chicken", "panda", "rabbit", "koala", "sea-lion", "giraffe", "puffer-fish", "penguin", "sloth", "dragon", "dog", "hen", "weasel"];
